6f222264d7612709cbf8bafc89ee76fbe58321ac
Harini <rharinia11@gmail.com>
2025-02-23 12:27:41 +0530

    Second Commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from Harini. This is the sample file. I am going to modify it.'));
+app.get('/', (req, res) => res.send('Hello from Harini. Let us see if the changes are seen'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
