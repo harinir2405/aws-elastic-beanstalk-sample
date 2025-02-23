7ce3040e236c31341b7f323011ef9c413821eac0
harinir2405 <117801238+harinir2405@users.noreply.github.com>
2025-02-23 08:46:37 +0530

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All! This is Farhan Akthar K. An Update is going to be made.This will be going to be another update.'));
-
+app.get('/', (req, res) => res.send('Hello from Harini. This is the sample file'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
